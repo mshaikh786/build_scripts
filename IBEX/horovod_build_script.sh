3a4f89e1c758c17a0898de37b82e5bd2c12d4c01
Mohsin A. Shaikh <shaima0d@login510-22.ibex.kaust.edu.sa>
2020-11-11 14:38:39 +0300

    typo PYHOROVOD to PYTORCH

--- IBEX/horovod_build_script.sh
+++ IBEX/horovod_build_script.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
-export HOROVOD_VERSION=0.19.0
-export TARBALL="/ibex/scratch/shaima0d/software/src/horovod-v${HOROVOD_VERSION}.tar.gz"
-export BLD_DIR="/ibex/scratch/shaima0d/software/build/horovod-${HOROVOD_VERSION}"
-export PREFIX="/ibex/scratch/shaima0d/software/apps/horovod/${HOROVOD_VERSION}"
+APP_NAME="horovod"
+
+if [ $# -eq 0 ]; then
+	source optargs.sh -h
+	exit
+else
+	source optargs.sh $@
+fi
+export HOROVOD_VERSION=$VERSION
+export SOFTWARE_ROOT=$BLD_ROOT
+export BLD_DIR="${SOFTWARE_ROOT}/build/${APP_NAME}-${HOROVOD_VERSION}"
+export PREFIX=$PREFIX
+export MODULEPATH=$MODULEPATH_PREPEND:$MODULEPATH
+if [ -z "${MAX_JOBS}" ];then
+		export MAX_JOBS=1
+fi
 
 function set_env(){
-	module load /sw/hidden/modulefiles/compilers/python/3.6.2
-	module load pytorch/1.3.1
-	module load cmake
+	module load cuda/10.2.89
+	module load pytorch/1.5.1
+	module load /ibex/scratch/shaima0d/software/modulefiles/cmake/3.16.1
 	module list
 }
 
 function get_source(){
-     	git clone --recursive -b v${HOROVOD_VERSION} https://github.com/horovod/horovod horovod-${HOROVOD_VERSION}
+     	git clone --recursive https://github.com/horovod/horovod horovod-${HOROVOD_VERSION}
 	if [ -d ${BLD_DIR} ]; then
 		cd ${BLD_DIR}
+		git fetch --tags --all
+		git checkout v${HOROVOD_VERSION}
 		# if you are updating an existing checkout
 		git submodule sync
 		git submodule update --init --recursive
 	fi
-	#wget https://github.com/horovod/horovod/archive/v${HOROVOD_VERSION}.tar.gz -O /ibex/scratch/shaima0d/software/src/horovod-v${HOROVOD_VERSION}.tar.gz
 }
 
 function build(){
-	#tar xvf ${TARBALL}
+	
 	cd ${BLD_DIR}
 	export CC=mpicc CXX=mpicxx
 	export HOROVOD_NCCL_INCLUDE=${NCCL_HOME}/include
@@ -32,25 +45,34 @@ function build(){
 	export HOROVOD_CUDA_HOME=${CUDATOOLKIT_HOME}
 	export HOROVOD_GPU_ALLREDUCE=NCCL HOROVOD_GPU_BROADCAST=NCCL HOROVOD_GPU_ALLGATHER=MPI
 	export HOROVOD_ALLOW_MIXED_GPU_IMPL=0
-	export HOROVOD_WITH_PYHOROVOD=1 
+	export HOROVOD_WITH_PYTORCH=1 
 	export HOROVOD_WITHOUT_TENSORFLOW=1
 	export HOROVOD_WITHOUT_MXNET=1
 	export HOROVOD_WITHOUT_GLOO=1
-	export MAX_JOBS=1
 	
-	mkdir -p ${PREFIX}/lib/python3.6/site-packages
-	export PYTHONPATH=${PREFIX}/lib/python3.6/site-packages:$PYTHONPATH
+	
+	mkdir -p ${PREFIX}/lib/python${PYTHON_MAJ_MIN_VER}/site-packages
+	export PYTHONPATH=${PREFIX}/lib/python${PYTHON_MAJ_MIN_VER}/site-packages:$PYTHONPATH
 	python setup.py install --prefix=$PREFIX
 }
 
-ARG=$1
-if [ ${ARG} = "all" ] || [ "${ARG}x" = "x" ]
+if [ ${ACTION} = "all" ]
  then
 	set_env
 	get_source
 	build
-elif [ ${ARG} = "build" ] 
+elif [ ${ACTION} = "build" ] 
  then
 	set_env
 	build
+elif [ ${ACTION} = "dryrun" ]
+ then
+	echo $HOROVOD_VERSION
+	echo $BLD_DIR
+	echo $PREFIX
+	echo $MODULEPATH
+
+else 
+	echo "Unrecognized ACTION"
+	exit
 fi
